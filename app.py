56f88bb1c475b5181f6bc705732284324bc61951
InezaMbabazi <106543808+InezaMbabazi@users.noreply.github.com>
2024-10-03 11:35:29 +0200

    Update app.py

--- app.py
+++ app.py
@@ -6,7 +6,7 @@ import matplotlib.pyplot as plt
 
 # Load the trained model
 try:
-    model = joblib.load('model = joblib.load('./path_to_directory/student_model.pkl')
+    model = joblib.load('./path_to_directory/student_model.pkl')
 
 except FileNotFoundError:
     st.error("Model not found. Please train and save the model first.")
