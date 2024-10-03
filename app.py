20fa20d4d6f336a23c444d3d39410963e4455c2e
InezaMbabazi <106543808+InezaMbabazi@users.noreply.github.com>
2024-10-03 11:40:01 +0200

    Update app.py

--- app.py
+++ app.py
@@ -6,7 +6,8 @@ import matplotlib.pyplot as plt
 
 # Load the trained model
 try:
-    model = joblib.load('./prediction/student_model.pkl')
+   model = joblib.load('student_model.pkl')
+
 
 
 except FileNotFoundError:
